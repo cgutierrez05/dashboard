afbd5db96a79bf4d9550758bb8a4e741828fd3a9
cgutierrez05 <carlagutierrezc05@gmail.com>
2025-12-10 18:24:52 +0000

    guia16 terminada

--- src/componentes/ChartUI.tsx
+++ src/componentes/ChartUI.tsx
@@ -1,12 +1,21 @@
 import { LineChart } from '@mui/x-charts/LineChart';
 import Typography from '@mui/material/Typography';
+import useFetchData from '../functions/useFetchData';
 
-const arrValues1 = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const arrValues2 = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const arrLabels = ['A','B','C','D','E','F','G'];
-
+// const arrValues1 = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+// const arrValues2 = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+// const arrLabels = ['A','B','C','D','E','F','G'];
 
 export default function ChartUI() {
+    const resultado = useFetchData();
+
+    const fechas = resultado.data?.hourly.time ?? [];
+
+    const temp = resultado.data?.hourly.temperature_2m ?? [];
+
+    const windsp = resultado.data?.hourly.wind_speed_10m ?? [];
+
+    
     return (
         <>
             <Typography variant="h5" component="div">
@@ -15,10 +24,10 @@ export default function ChartUI() {
             <LineChart
                 height={300}
                 series={[
-                    { data: arrValues1, label: 'value1'},
-                    { data: arrValues2, label: 'value2'},
+                    { data: temp, label: 'value1'},
+                    { data: windsp, label: 'value2'},
                 ]}
-                xAxis={[{ scaleType: 'point', data: arrLabels }]}
+                xAxis={[{ scaleType: 'point', data: fechas }]}
             />
         </>
     );
